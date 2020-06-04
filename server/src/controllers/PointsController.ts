e7d83fa94f7c5b100d3bf4b7d431f21cda6d5ef8
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-06-04 17:18:54 -0300

    web

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -7,6 +7,8 @@ class PointsController {
   async index(request: Request, response: Response) {
     const { city, uf, items } = request.query;
 
+    console.log(city, uf, items);
+
     const parsedItems = String(items)
       .split(',')
       .map(item => Number(item.trim()));
